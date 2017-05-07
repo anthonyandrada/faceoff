16b49d82cec035aed6a1e8561c5e9282c2741c66
eddie5210200 <eddie5210200@yahoo.com>
2017-05-07 01:40:42 -0700

    Add files via upload

---
+++ eyeLike-master/build/src/CMakeFiles/eyeLike.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/syadmin/Downloads/eyeLike-master/src/findEyeCenter.cpp" "/home/syadmin/Downloads/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "/home/syadmin/Downloads/eyeLike-master/src/findEyeCorner.cpp" "/home/syadmin/Downloads/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "/home/syadmin/Downloads/eyeLike-master/src/helpers.cpp" "/home/syadmin/Downloads/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "/home/syadmin/Downloads/eyeLike-master/src/main.cpp" "/home/syadmin/Downloads/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ eyeLike-master/src/main.cpp
@@ -0,0 +1,213 @@
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+
+#include "constants.h"
+#include "findEyeCenter.h"
+#include "findEyeCorner.h"
+
+
+/** Constants **/
+
+
+/** Function Headers */
+void detectAndDisplay( cv::Mat frame );
+
+/** Global variables */
+//-- Needs opencv/data/haarscascades path
+cv::String face_cascade_name = "/usr/local/etc/OpenFace/classifiers/haarcascade_frontalface_alt.xml";
+cv::CascadeClassifier face_cascade;
+std::string main_window_name = "Capture - Face detection";
+std::string face_window_name = "Capture - Face";
+cv::RNG rng(12345);
+cv::Mat debugImage;
+cv::Mat skinCrCbHist = cv::Mat::zeros(cv::Size(256, 256), CV_8UC1);
+cv::Point currentLeftPupil = cv::Point(0,0);
+cv::Point currentRightPupil = cv::Point(0,0);
+
+/**
+ * @function main
+ */
+int main( int argc, const char** argv ) {
+   //if no arguments
+  if(argc > 1) {
+  printf("address: %s" , argv[1]);
+   //Demo image
+  cv::String address = argv[1];
+
+  cv::Mat frame = cv::imread(address);
+  // Load the cascades
+  if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade, please change face_cascade_name in source code.\n"); return -1; };
+  
+  createCornerKernels();
+  ellipse(skinCrCbHist, cv::Point(113, 155.6), cv::Size(23.4, 15.2),
+          43.0, 0.0, 360.0, cv::Scalar(255, 255, 255), -1);
+      // mirror it
+      frame.copyTo(debugImage);
+      // Apply the classifier to the frame
+      if( !frame.empty() ) {
+        detectAndDisplay( frame );
+      }
+      else {
+        printf(" --(!) No image -- Break!");
+      }
+    
+  releaseCornerKernels();
+  // Testers, check for eyes - remove later
+  line(frame, cv::Point(0,0), currentLeftPupil, 1234);
+  line(frame, cv::Point(0,0), currentRightPupil, 1234);
+  imwrite("/home/syadmin/Downloads/testTemp.png", frame);
+ 
+  /*
+   * Add to Database
+   * Point - currentLeftPupil
+   * Point - currentRightPupil
+   */
+  } else {printf("no input"); }
+  return 0;
+}
+
+void findEyes(cv::Mat frame_gray, cv::Rect face) {
+  cv::Mat faceROI = frame_gray(face);
+  cv::Mat debugFace = faceROI;
+
+  if (kSmoothFaceImage) {
+    double sigma = kSmoothFaceFactor * face.width;
+    GaussianBlur( faceROI, faceROI, cv::Size( 0, 0 ), sigma);
+  }
+  //-- Find eye regions and draw them
+  int eye_region_width = face.width * (kEyePercentWidth/100.0);
+  int eye_region_height = face.width * (kEyePercentHeight/100.0);
+  int eye_region_top = face.height * (kEyePercentTop/100.0);
+  cv::Rect leftEyeRegion(face.width*(kEyePercentSide/100.0),
+                         eye_region_top,eye_region_width,eye_region_height);
+  cv::Rect rightEyeRegion(face.width - eye_region_width - face.width*(kEyePercentSide/100.0),
+                          eye_region_top,eye_region_width,eye_region_height);
+  //Second Readjustments
+  currentLeftPupil = cv::Point(currentLeftPupil.x + face.width*(kEyePercentSide/100.0), currentLeftPupil.y + eye_region_top); 
+  currentRightPupil = cv::Point(currentRightPupil.x + face.width - eye_region_width - face.width*(kEyePercentSide/100.0), currentRightPupil.y + eye_region_top); 
+
+  //-- Find Eye Centers
+  cv::Point leftPupil = findEyeCenter(faceROI,leftEyeRegion,"Left Eye");
+  cv::Point rightPupil = findEyeCenter(faceROI,rightEyeRegion,"Right Eye");
+  // Final Readjustments
+  currentLeftPupil = cv::Point(currentLeftPupil.x + leftPupil.x, currentLeftPupil.y + leftPupil.y); 
+  currentRightPupil = cv::Point(currentRightPupil.x + rightPupil.x, currentRightPupil.y + rightPupil.y); 
+  // get corner regions
+  cv::Rect leftRightCornerRegion(leftEyeRegion);
+  leftRightCornerRegion.width -= leftPupil.x;
+  leftRightCornerRegion.x += leftPupil.x;
+  leftRightCornerRegion.height /= 2;
+  leftRightCornerRegion.y += leftRightCornerRegion.height / 2;
+  cv::Rect leftLeftCornerRegion(leftEyeRegion);
+  leftLeftCornerRegion.width = leftPupil.x;
+  leftLeftCornerRegion.height /= 2;
+  leftLeftCornerRegion.y += leftLeftCornerRegion.height / 2;
+  cv::Rect rightLeftCornerRegion(rightEyeRegion);
+  rightLeftCornerRegion.width = rightPupil.x;
+  rightLeftCornerRegion.height /= 2;
+  rightLeftCornerRegion.y += rightLeftCornerRegion.height / 2;
+  cv::Rect rightRightCornerRegion(rightEyeRegion);
+  rightRightCornerRegion.width -= rightPupil.x;
+  rightRightCornerRegion.x += rightPupil.x;
+  rightRightCornerRegion.height /= 2;
+  rightRightCornerRegion.y += rightRightCornerRegion.height / 2;
+  rectangle(debugFace,leftRightCornerRegion,200);
+  rectangle(debugFace,leftLeftCornerRegion,200);
+  rectangle(debugFace,rightLeftCornerRegion,200);
+  rectangle(debugFace,rightRightCornerRegion,200);
+  // change eye centers to face coordinates
+  rightPupil.x += rightEyeRegion.x;
+  rightPupil.y += rightEyeRegion.y;
+  leftPupil.x += leftEyeRegion.x;
+  leftPupil.y += leftEyeRegion.y;
+  // draw eye centers
+  circle(debugFace, rightPupil, 3, 1234);
+  circle(debugFace, leftPupil, 3, 1234);
+  //-- Find Eye Corners
+  if (kEnableEyeCorner) {
+    cv::Point2f leftRightCorner = findEyeCorner(faceROI(leftRightCornerRegion), true, false);
+    leftRightCorner.x += leftRightCornerRegion.x;
+    leftRightCorner.y += leftRightCornerRegion.y;
+    cv::Point2f leftLeftCorner = findEyeCorner(faceROI(leftLeftCornerRegion), true, true);
+    leftLeftCorner.x += leftLeftCornerRegion.x;
+    leftLeftCorner.y += leftLeftCornerRegion.y;
+    cv::Point2f rightLeftCorner = findEyeCorner(faceROI(rightLeftCornerRegion), false, true);
+    rightLeftCorner.x += rightLeftCornerRegion.x;
+    rightLeftCorner.y += rightLeftCornerRegion.y;
+    cv::Point2f rightRightCorner = findEyeCorner(faceROI(rightRightCornerRegion), false, false);
+    rightRightCorner.x += rightRightCornerRegion.x;
+    rightRightCorner.y += rightRightCornerRegion.y;
+    circle(faceROI, leftRightCorner, 3, 200);
+    circle(faceROI, leftLeftCorner, 3, 200);
+    circle(faceROI, rightLeftCorner, 3, 200);
+    circle(faceROI, rightRightCorner, 3, 200);
+  }
+    //Test pupils found
+ // imwrite("/home/syadmin/Downloads/testTemp.png", faceROI);
+//  cv::Rect roi( cv::Point( 0, 0 ), faceROI.size());
+//  cv::Mat destinationROI = debugImage( roi );
+//  faceROI.copyTo( destinationROI );
+}
+
+
+cv::Mat findSkin (cv::Mat &frame) {
+  cv::Mat input;
+  cv::Mat output = cv::Mat(frame.rows,frame.cols, CV_8U);
+
+  cvtColor(frame, input, CV_BGR2YCrCb);
+
+  for (int y = 0; y < input.rows; ++y) {
+    const cv::Vec3b *Mr = input.ptr<cv::Vec3b>(y);
+//    uchar *Or = output.ptr<uchar>(y);
+    cv::Vec3b *Or = frame.ptr<cv::Vec3b>(y);
+    for (int x = 0; x < input.cols; ++x) {
+      cv::Vec3b ycrcb = Mr[x];
+//      Or[x] = (skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) > 0) ? 255 : 0;
+      if(skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) == 0) {
+        Or[x] = cv::Vec3b(0,0,0);
+      }
+    }
+  }
+  return output;
+}
+
+/**
+ * @function detectAndDisplay
+ */
+void detectAndDisplay( cv::Mat frame ) {
+  std::vector<cv::Rect> faces;
+  //cv::Mat frame_gray;
+
+  std::vector<cv::Mat> rgbChannels(3);
+  cv::split(frame, rgbChannels);
+  cv::Mat frame_gray = rgbChannels[2];
+
+  //cvtColor( frame, frame_gray, CV_BGR2GRAY );
+  //equalizeHist( frame_gray, frame_gray );
+  //cv::pow(frame_gray, CV_64F, frame_gray);
+  //-- Detect faces
+  face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT, cv::Size(150, 150) );
+ 
+//  findSkin(debugImage);
+  for( int i = 0; i < faces.size(); i++ )
+  {
+    rectangle(debugImage, faces[i], 1234);
+  }
+
+  //First Readjustment
+  currentLeftPupil = cv::Point(currentLeftPupil.x + faces[0].x, currentLeftPupil.y + faces[0].y); 
+  currentRightPupil = cv::Point(currentRightPupil.x + faces[0].x, currentRightPupil.y + faces[0].y); 
+  
+  //-- Show what you got
+  if (faces.size() > 0) {
+    findEyes(frame_gray, faces[0]);
+  }
+}
+
+
